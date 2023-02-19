6241a8fd48eedcd32db2d2e9a4bfd92a810c6e64
zherushen <zherushen2023@u.northwestern.edu>
2023-02-19 19:46:47 +0000

    1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,18 +1,40 @@
 class PlacesController < ApplicationController
 
     def index
-        @places = Place.all
-      end
+      @places = Place.all
+    end
     
-      def new
-        @place = Place.new
-      end
+    def show
+      @place = Place.find_by({"id" => params["id"]})
+      @posts = Post.where({"place_id" => @place["id"]})
+    end
+
+    def new
+      @place = Place.new
+    end
     
-      def create
-        @place = Post.new
-        @place["name"] = params["place"]["name"]
-        @post.save
-        redirect_to "/places"
-      end
+    def create
+      @place = Place.new
+      @place["name"] = params["place"]["name"]
+      @place.save
+      redirect_to "/places"
+    end
+
+    def edit
+      @place = Place.find_by({ "id" => params["id"] })
+    end
+  
+    def update
+      @place = Place.find_by({ "id" => params["id"] })
+      @place["name"] = params["place"]["name"]
+      @place.save
+      redirect_to "/places"
+    end
+  
+    def destroy
+      @place = Place.find_by({ "id" => params["id"] })
+      @place.destroy
+      redirect_to "/places"
+    end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,43 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+    
+    def new
+        @post = Post.new
+    end
+
+    def show
+        @post = Post.where({"place_id" => @place["id"]})
+    end
+
+    def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/places/#{@post["place_id"]}"
+    end
+
+    def edit
+        @post = Post.where({"place_id" => @place["id"]})
+    end
+    
+    def update
+        @post = Post.where({"place_id" => @place["id"]})
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/places/#{@post["place_id"]}"
+    end
+    
+    def destroy
+        @post = Post.where({"place_id" => @place["id"]})
+        @post.destroy
+        redirect_to "/posts"
+    end
+  
 end
