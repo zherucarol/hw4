a8c57c5bb1232cf9b5c7203bc77a261360abcc44
zherushen <zherushen2023@u.northwestern.edu>
2023-02-20 00:30:10 +0000

    finished

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,21 +20,4 @@ class PlacesController < ApplicationController
       redirect_to "/places"
     end
 
-    def edit
-      @place = Place.find_by({ "id" => params["id"] })
-    end
-  
-    def update
-      @place = Place.find_by({ "id" => params["id"] })
-      @place["name"] = params["place"]["name"]
-      @place.save
-      redirect_to "/places"
-    end
-  
-    def destroy
-      @place = Place.find_by({ "id" => params["id"] })
-      @place.destroy
-      redirect_to "/places"
-    end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,6 @@ class PostsController < ApplicationController
         @post["place_id"] = @place["id"]
     end
 
-    def show
-        @post = Post.find_by({ "id" => params["id"] })
-        @place = Place.find_by({ "id" => @post["place_id"] })
-    end
-
     def create
         @post = Post.new
         @post["title"] = params["post"]["title"]
@@ -23,24 +18,5 @@ class PostsController < ApplicationController
         @post.save
         redirect_to "/places/#{@post["place_id"]}"
     end
-
-    def edit
-        @post = Post.where({"place_id" => @place["id"]})
-    end
-    
-    def update
-        @post = Post.where({"place_id" => @place["id"]})
-        @post["description"] = params["post"]["description"]
-        @post["posted_on"] = params["post"]["posted_on"]
-        @post["place_id"] = params["post"]["place_id"]
-        @post.save
-        redirect_to "/places/#{@post["place_id"]}"
-    end
-    
-    def destroy
-        @post = Post.where({"place_id" => @place["id"]})
-        @post.destroy
-        redirect_to "/posts"
-    end
-  
+ 
 end
