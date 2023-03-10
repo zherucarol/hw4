b554cb8b51621539ecd69e363936b7f26e03eb37
zherucarol shen <carolszr@outlook.com>
2023-03-10 01:56:06 +0000

    1

--- db/migrate/20220210225503_create_posts.rb
+++ db/migrate/20220210225503_create_posts.rb
@@ -6,6 +6,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.date "posted_on"
       t.integer "place_id"
       t.integer "user_id"
+      t.string :image
       
       t.timestamps
     end
