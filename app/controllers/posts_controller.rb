ab8f8436dc90454672d4344af59ba56e6ec186a9
zherucarol shen <carolszr@outlook.com>
2023-03-10 00:14:35 +0000

    1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,20 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new
-        @post["title"] = params["post"]["title"]
-        @post["description"] = params["post"]["description"]
-        @post["posted_on"] = params["post"]["posted_on"]
-        @post["place_id"] = params["post"]["place_id"]
-        @post["user_id"] = session["user_id"]
-        @post.save
-        redirect_to "/places/#{@post["place_id"]}"
+        if @current_user
+            @post = Post.new
+            @post["title"] = params["post"]["title"]
+            @post["description"] = params["post"]["description"]
+            @post["posted_on"] = params["post"]["posted_on"]
+            @post["place_id"] = params["post"]["place_id"]
+            @post.uploaded_image.attach(params["post"]["uploaded_image"])
+            @post["user_id"] = @current_user["id"]
+            @post.save
+        else
+            flash["notice"] = "Login first."
+        end
+            redirect_to "/places/#{@post["place_id"]}"
     end
+        
  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   # talks to the "posts" table
+  has_one_attached :uploaded_image
 end
