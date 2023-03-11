fbb7a088b1ce4e25590d0ee12ddfc7fe35816dd0
zherucarol shen <carolszr@outlook.com>
2023-03-11 04:36:51 +0000

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
 
     def current_user
       @current_user = User.find_by({ "id" => session["user_id"] })
+      puts "------------------ code before every request ------------------"
     end
 end
