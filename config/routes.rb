e667c639afc50c61507cdcdbd9e1ad5be3e3ce61
zherucarol shen <carolszr@outlook.com>
2023-03-11 05:12:13 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources "places"
-  get("/", { :controller => "places", :action => "index" })
   resources "posts"
-  get("/", { :controller => "posts", :action => "index" })
   resources "users"
   resources "sessions"
   
